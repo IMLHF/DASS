21da214e0197baefe426c7e5cc469de5c459912f
IMLHF <red_wind@foxmail.com>
2018-11-10 23:25:25 +0800

    #fix PIT_MSE_LOSS

--- losses/loss.py
+++ losses/loss.py
@@ -6,7 +6,17 @@ def MSE(y1, y2):
   return tf.reduce_mean(tf.square(tf.subtract(y1, y2)))
 
 
-def PIT_MSE(y1, y2):
+def PIT_MSE_for_CNN(y1, y2):
+  # for i in range(tf.shape(y1)[0]):
+  loss1=tf.reduce_mean(tf.square(tf.subtract(y1, y2)),[1,2])
+  y1_speaker1, y1_speaker2 = tf.split(y1, 2, axis=-1)
+  y1_swaped = tf.concat([y1_speaker2, y1_speaker1], axis=-1)
+  loss2=tf.reduce_mean(tf.square(tf.subtract(y1_swaped, y2)),[1,2])
+  loss=tf.where(loss1<loss2,loss1,loss2)
+  return tf.reduce_mean(loss)
+
+
+def PIT_MSE_abandon(y1, y2):
   # np.shape(y1)
   axis = len(np.shape(y1))-1
   y1_speaker1, y1_speaker2 = tf.split(y1, 2, axis=axis)
