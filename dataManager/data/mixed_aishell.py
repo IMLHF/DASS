90eb23738ef6f25862bd5f73fb8719abd11bc84b
IMLHF <red_wind@foxmail.com>
2018-11-28 20:36:23 +0800

    # move mixed_aishell param to overall FLAGS

--- dataManager/data/mixed_aishell.py
+++ dataManager/data/mixed_aishell.py
@@ -8,17 +8,21 @@ import scipy.io
 import wave
 import numpy as np
 from utils import spectrum_tool
+from FLAGS import MIXED_AISHELL_PARAM
 
 
-# region define
 FILE_NAME = __file__[max(__file__.rfind('/')+1, 0):__file__.rfind('.')]
-LOG_NORM_MAX = 5
-LOG_NORM_MIN = -3
-NFFT = 512
-OVERLAP = 256
-FS = 16000
-LEN_WAWE_PAD_TO = 160000
-WAVE_NORM=True
+# region define
+LOG_NORM_MAX = MIXED_AISHELL_PARAM.LOG_NORM_MAX
+LOG_NORM_MIN = MIXED_AISHELL_PARAM.LOG_NORM_MIN
+NFFT = MIXED_AISHELL_PARAM.NFFT
+OVERLAP = MIXED_AISHELL_PARAM.OVERLAP
+FS = MIXED_AISHELL_PARAM.FS
+LEN_WAWE_PAD_TO = MIXED_AISHELL_PARAM.LEN_WAWE_PAD_TO
+UTT_SEG_FOR_MIX= MIXED_AISHELL_PARAM.UTT_SEG_FOR_MIX
+DATASET_DIRS = MIXED_AISHELL_PARAM.DATASET_DIRS
+DATASET_SIZES = MIXED_AISHELL_PARAM.DATASET_SIZES
+WAVE_NORM = MIXED_AISHELL_PARAM.WAVE_NORM
 # endregion
 
 
@@ -31,14 +35,14 @@ def _get_waveData1__waveData2(file1, file2):
                             dtype=np.int16)
   f1.close()
   f2.close()
-  while len(waveData1)<LEN_WAWE_PAD_TO:
-    waveData1=np.tile(waveData1,2)
-  while len(waveData2)<LEN_WAWE_PAD_TO:
-    waveData2=np.tile(waveData2,2)
+  while len(waveData1) < LEN_WAWE_PAD_TO:
+    waveData1 = np.tile(waveData1, 2)
+  while len(waveData2) < LEN_WAWE_PAD_TO:
+    waveData2 = np.tile(waveData2, 2)
 
   if WAVE_NORM:
-    waveData1=waveData1/np.max(np.abs(waveData1)) * 32767
-    waveData2=waveData2/np.max(np.abs(waveData2)) * 32767
+    waveData1 = waveData1/np.max(np.abs(waveData1)) * 32767
+    waveData2 = waveData2/np.max(np.abs(waveData2)) * 32767
   # if len(waveData1) < len(waveData2):
   #   waveData1, waveData2 = waveData2, waveData1
   # # print(np.shape(waveData1))
@@ -54,10 +58,12 @@ def _mix_wav(waveData1, waveData2):
   mixedData = np.array(mixedData, dtype=np.int16)  # 必须指定是16位，因为写入音频时写入的是二进制数据
   return mixedData
 
+
 def rmNormalization(tmp):
-  tmp=(10**(tmp*(LOG_NORM_MAX-LOG_NORM_MIN)+LOG_NORM_MIN))-0.5
-  ans=np.where(tmp>0,tmp,0) # 防止计算误差导致的反归一化结果为负数
+  tmp = (10**(tmp*(LOG_NORM_MAX-LOG_NORM_MIN)+LOG_NORM_MIN))-0.5
+  ans = np.where(tmp > 0, tmp, 0)  # 防止计算误差导致的反归一化结果为负数
   return ans
+
 
 def _extract_norm_log_mag_spec(data):
   # 归一化的幅度谱对数
@@ -74,9 +80,11 @@ def _extract_norm_log_mag_spec(data):
   # log_mag_spec=(log_mag_spec-mean)/var
   return log_mag_spec
 
+
 def _extract_phase(data):
-  theta=spectrum_tool.phase_spectrum_librosa_stft(data,NFFT,OVERLAP)
+  theta = spectrum_tool.phase_spectrum_librosa_stft(data, NFFT, OVERLAP)
   return theta
+
 
 def _extract_feature_x(data_index_list):
   data = []
@@ -118,6 +126,7 @@ def _extract_feature_x_y(data_index_list):
     # print(np.shape(datay))
   return [np.array(datax), np.array(datay)]
 
+
 def _extract_x_theta(data_index_list):
   data = []
   for data_index in data_index_list:
@@ -155,15 +164,15 @@ def _init_data__(rawdata, data_dict_dir, logger):
     if os.path.isdir(speaker_dir):
       speaker_wav_list = os.listdir(speaker_dir)
       speaker_wav_list.sort()
-      for wav in speaker_wav_list[:260]:
+      for wav in speaker_wav_list[:UTT_SEG_FOR_MIX[0]]:
         if wav[-4:] == ".wav":
           cwl_train_file.write(speaker_dir+'/'+wav+'\n')
           clean_wav_list_train.append(speaker_dir+'/'+wav)
-      for wav in speaker_wav_list[260:300]:
+      for wav in speaker_wav_list[UTT_SEG_FOR_MIX[0]:UTT_SEG_FOR_MIX[1]]:
         if wav[-4:] == ".wav":
           cwl_validation_file.write(speaker_dir+'/'+wav+'\n')
           clean_wav_list_validation.append(speaker_dir+'/'+wav)
-      for wav in speaker_wav_list[-30:]:
+      for wav in speaker_wav_list[UTT_SEG_FOR_MIX[1]:]:
         if wav[-4:] == ".wav":
           cwl_test_cc_file.write(speaker_dir+'/'+wav+'\n')
           clean_wav_list_test_cc.append(speaker_dir+'/'+wav)
@@ -175,8 +184,9 @@ def _init_data__(rawdata, data_dict_dir, logger):
   logger.print_save('validation clean: '+str(len(clean_wav_list_validation)))
   logger.print_save('test_cc clean: '+str(len(clean_wav_list_test_cc)))
 
-  data_class_dir = ['train', 'validation', 'test_cc']
-  all_mixed=0
+  data_class_dir = DATASET_DIRS
+  dataset_mixedutt_num = DATASET_SIZES
+  all_mixed = 0
   for (clean_wav_list, j) in zip((clean_wav_list_train, clean_wav_list_validation, clean_wav_list_test_cc), range(3)):
     logger.print_save('\n'+data_class_dir[j]+" data preparing...")
     logger.print_save('Current time: ' +
@@ -184,26 +194,39 @@ def _init_data__(rawdata, data_dict_dir, logger):
     mixed_wav_list_file = open(
         data_dict_dir+'/'+data_class_dir[j]+'/mixed_wav_dir.list', 'a+')
     mixed_wave_list = []
-    len_wav_list=len(clean_wav_list)
-    for i_utt in range(len_wav_list):
-      for j_utt in range(i_utt,len_wav_list):
-        utt1_dir=clean_wav_list[i_utt]
-        utt2_dir=clean_wav_list[j_utt]
-        speaker1 = utt1_dir.split('/')[-2]
-        speaker2 = utt2_dir.split('/')[-2]
-        if speaker1 == speaker2:
-          continue
-        mixed_wav_list_file.write(utt1_dir+' '+utt2_dir+'\n')
-        mixed_wave_list.append([utt1_dir, utt2_dir])
+    len_wav_list = len(clean_wav_list)
+    generated_num = 0
+    while generated_num < dataset_mixedutt_num[j]:
+      uttid = np.random.randint(len_wav_list, size=2)
+      uttid1 = uttid[0]
+      uttid2 = uttid[1]
+      utt1_dir = clean_wav_list[uttid1]
+      utt2_dir = clean_wav_list[uttid2]
+      speaker1 = utt1_dir.split('/')[-2]
+      speaker2 = utt2_dir.split('/')[-2]
+      if speaker1 == speaker2:
+        continue
+      generated_num += 1
+      mixed_wav_list_file.write(utt1_dir+' '+utt2_dir+'\n')
+      mixed_wave_list.append([utt1_dir, utt2_dir])
+    # for i_utt in range(len_wav_list): # n^2混合，数据量巨大
+    #   for j_utt in range(i_utt,len_wav_list):
+    #     utt1_dir=clean_wav_list[i_utt]
+    #     utt2_dir=clean_wav_list[j_utt]
+    #     speaker1 = utt1_dir.split('/')[-2]
+    #     speaker2 = utt2_dir.split('/')[-2]
+    #     if speaker1 == speaker2:
+    #       continue
+    #     mixed_wav_list_file.write(utt1_dir+' '+utt2_dir+'\n')
+    #     mixed_wave_list.append([utt1_dir, utt2_dir])
     mixed_wav_list_file.close()
     scipy.io.savemat(
         data_dict_dir+'/'+data_class_dir[j]+'/mixed_wav_dir.mat', {"mixed_wav_dir": mixed_wave_list})
-    all_mixed+=len(mixed_wave_list)
+    all_mixed += len(mixed_wave_list)
     logger.print_save(data_class_dir[j]+' mixed: '+str(len(mixed_wave_list)))
   logger.print_save('All : '+str(all_mixed))
   logger.print_save(
       '\nData preparation over time: '+str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-
 
 
 # API
@@ -222,7 +245,7 @@ def read_data_sets(rawdata):
   itemgetor_list = [_extract_feature_x,
                     _extract_feature_y,
                     _extract_feature_x_y,
-                    _extract_x_theta,]
+                    _extract_x_theta, ]
   data = DATABASE(set_dict, itemgetor_list)
   data.train
   return data
